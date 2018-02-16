4787a503493c592243ab4bdbcf840221c8b18d2d
colinjnash <colinjnash@gmail.com>
2018-02-15 19:30:46 -0500

    Prod

--- server.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const app = express();
-const path= require('path');
-const port = process.env.PORT || 8080;
-
-// Since the root/src dir contains our index.html
-app.use(express.static(__dirname + '/build'));
-
-app.listen(port, () => console.log(`Server started on ${port}`));
