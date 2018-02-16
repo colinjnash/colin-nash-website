835812a1eb929fb2eb39e6953de135ce0f14471e
colinjnash <colinjnash@gmail.com>
2018-02-15 20:51:15 -0500

    Kill me

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.logger('dev'));
 app.use(express.compress());
 app.use(express.static(__dirname + '/build'));////
 
