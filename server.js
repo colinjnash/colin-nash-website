bd3c1bc621c3ea52b06ecfef9dcac0c4a4b4f7ad
colinjnash <colinjnash@gmail.com>
2018-02-15 21:25:03 -0500

    Nick is a God

--- server.js
+++ server.js
@@ -5,5 +5,9 @@ app.set('port', process.env.PORT || 3000);
 
 app.use(express.static(__dirname + '/build'));////
 
+app.get('/', function(req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
 app.listen(app.get('port'), function() {
 });
