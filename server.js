588012ef807061725518a08006426c984a11a9ec
colinjnash <colinjnash@gmail.com>
2018-02-15 21:00:26 -0500

    Hell

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ const app = express();
 
 app.set('port', process.env.PORT || 3000);
 
-app.use(express.compress());
 app.use(express.static(__dirname + '/build'));////
 
 app.listen(app.get('port'), function() {
