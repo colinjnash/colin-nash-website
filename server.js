40f7d15ce8bee72315898c0ffaa80d1cea032411
colinjnash <colinjnash@gmail.com>
2018-02-15 19:03:02 -0500

    This is hell

--- server.js
+++ server.js
@@ -6,7 +6,4 @@ const port = process.env.PORT || 8080;
 // Since the root/src dir contains our index.html
 app.use(express.static(__dirname + '/build'));
 
-app.get('*', function (request, response){
-  response.sendFile(path.resolve(__dirname, 'build', 'index.html'));
-});
 app.listen(port, () => console.log(`Server started on ${port}`));
