1fa4a4df31311acf19e93b78e3f7796b03f967b8
Colin Nash <colinjnash@gmail.com>
2018-03-05 16:11:04 -0500

    Safari styling change

--- src/styles/contactStyles.js
+++ src/styles/contactStyles.js
@@ -2,7 +2,7 @@ import styled, { css, keyframes } from 'styled-components';
 
 
 export const AboutTitle = styled.h2`
-grid-column: 1/4;
+grid-column: 1/5;
 font-weight: 200;
 font-size: 2em;
 position: flex;
@@ -16,11 +16,12 @@ export const ContactP = styled.p`
 font-family: 'icomoon';
 justify-self: center;
 font-size: 3em;
+display: flex;
 &:hover{
 	color: red;
 	transition: color 300ms;
 }
-`
+`;
 
 export const ContactLink = styled.a`
 text-decoration: none;
