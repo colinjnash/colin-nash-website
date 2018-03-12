79c1ba32894eb5824a2116cc8e2f8b424bcd705d
colinjnash <colinjnash@gmail.com>
2018-03-11 23:16:00 -0400

    React basic setup

--- src/Index.js
+++ src/Index.js
@@ -1,16 +1,20 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import { render } from 'react-dom';
 import { createStore } from 'redux';
-import { Provider } from 'react-redux';
 import { rootReducer } from './reducers/rootReducer';
 import registerServiceWorker from './registerserviceworker';
+import Root from './components/Root';
 
 
 
 const store = createStore(rootReducer);
 
-ReactDOM.render(<Provider store={store}><App /></Provider>, document.querySelector('#container'));
+
+render(
+  <Root store={store} />,
+  document.querySelector('#container')
+);
+
 
 registerServiceWorker();
 // Only for HTTPS connections. Will work around.
